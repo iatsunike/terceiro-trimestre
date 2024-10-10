5e756b9ad0913a3de578b63d70027ac583a34313
iatsunike <164032522+iatsunike@users.noreply.github.com>
2024-10-10 11:36:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function criarCartao(categoria, pergunta,resposta){
+    let container = document.getElementById('container')
+    let cartao = doscument.createElement('article')
+    cartao.className = 'cartao'
+
+cartao.innerHTML = `
+<div class="cartao_conteudo">
+        <h3>$(categoria)</h3>
+        <div class= "cartao_conteudo_pergunta">
+            <p>$(pergunta)</p>
+        </div>
+        <div class= "cartao_conteudo_resposta">
+             <p>$(resposta)</p>
+        </div>
+    </div>`
+    container.appendChild(cartao)
+}
